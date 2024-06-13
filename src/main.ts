a74e51402a17c019898d5fec893f90958225c48a
Guste Gaubaite <219.guste@gmail.com>
2024-06-13 09:33:39 +0200

    Ejercicio Módulo 2 completado

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+const totalCena: number = 120;
+const totalBebidas: number = 18;
+const numAmigos: number = 6;
+
+const costoPorCubrir: number = totalCena - totalBebidas;
+
+const costoPorPersona: number = costoPorCubrir / numAmigos;
+
+console.log(`Cada comensal deberá pagar ${costoPorPersona}€.`);
