ac6022064ab56b223805f151b32022dad09a9269
Guste Gaubaite <219.guste@gmail.com>
2024-06-15 19:31:08 +0200

    Completada entrega del Módulo 3

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,80 @@
 import "./style.css";
 
-const totalCena: number = 120;
-const totalBebidas: number = 18;
-const numAmigos: number = 6;
+interface GrupoMusical {
+    nombre: string;
+    añoDeFundacion: number;
+    activo: boolean;
+    generoMusical: string;
+}
 
-const costoPorCubrir: number = totalCena - totalBebidas;
+const popRock = "🎵 Pop Rock";
+const rock = "🎸 Rock";
+const hardRock = "🤘 Hard Rock";
+const clasica = "🎼 Clásica";
 
-const costoPorPersona: number = costoPorCubrir / numAmigos;
+const estiloTitulo = "font-weight:bold; font-size:16px; background-color:green";
 
-console.log(`Cada comensal deberá pagar ${costoPorPersona}€.`);
+const primerGrupoMusical: GrupoMusical  = {
+    nombre: "The Beatles",
+    añoDeFundacion: 1960,
+    activo: true,
+    generoMusical: popRock
+}
+
+const segundoGrupoMusical: GrupoMusical = {
+    nombre: "Queen",
+    añoDeFundacion: 1970,
+    activo: false,
+    generoMusical: rock
+}
+
+const tercerGrupoMusical: GrupoMusical = {
+    nombre: "AC DC",
+    añoDeFundacion: 1973,
+    activo: true,
+    generoMusical: hardRock
+}
+
+const compositor: GrupoMusical = {
+    nombre: "Ludwig van Beethoven",
+    añoDeFundacion: 1770,
+    activo: false,
+    generoMusical: clasica
+}
+
+const cuartoGrupoMusical: GrupoMusical = {
+    nombre: "The Rolling Stones",
+    añoDeFundacion: 1962,
+    activo: true,
+    generoMusical: rock
+}
+
+console.log(`%c${primerGrupoMusical.nombre}`, estiloTitulo);
+console.log(`se fundó en ${primerGrupoMusical.añoDeFundacion}
+activo: ${primerGrupoMusical.activo}
+su género musical: ${primerGrupoMusical.generoMusical}`
+);
+
+console.log(`%c${segundoGrupoMusical.nombre}`, estiloTitulo);
+console.log(`se fundó en ${segundoGrupoMusical.añoDeFundacion}
+activo: ${segundoGrupoMusical.activo}
+su género musical: ${segundoGrupoMusical.generoMusical}`
+);
+
+console.log(`%c${tercerGrupoMusical.nombre}`, estiloTitulo);
+console.log(`se fundó en ${tercerGrupoMusical.añoDeFundacion}
+activo: ${tercerGrupoMusical.activo}
+su género musical: ${tercerGrupoMusical.generoMusical}`
+);
+
+console.log(`%c${compositor.nombre}`, estiloTitulo);
+console.log(`nació en ${compositor.añoDeFundacion}
+activo: ${compositor.activo}
+su género musical: ${compositor.generoMusical}`
+);
+
+console.log(`%c${cuartoGrupoMusical.nombre}`, estiloTitulo);
+console.log(`se fundó en ${cuartoGrupoMusical.añoDeFundacion}
+activo: ${cuartoGrupoMusical.activo}
+su género musical: ${cuartoGrupoMusical.generoMusical}`
+);
