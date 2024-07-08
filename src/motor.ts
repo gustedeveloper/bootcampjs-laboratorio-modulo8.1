88e4576ca28246a427057ffc68216b7ea6c4c7f2
Guste Gaubaite <219.guste@gmail.com>
2024-07-08 09:54:40 +0200

    Creado archivo motor para englobar el código de las reglas y lógica del juego

---
+++ src/motor.ts
@@ -0,0 +1,41 @@
+import {partida, Estado} from './model';
+
+export const consigueNumeroRandom = () : number => Math.floor(Math.random() * 10) + 1;
+
+export const obtenerCarta = (numeroAleatorio: number) : number => {
+    if (numeroAleatorio > 7) {
+        return numeroAleatorio + 2;
+    }
+    return numeroAleatorio;
+  }
+
+export const obtenerPuntos = (carta: number) : number => {
+    return carta > 7 ? 0.5 : carta;
+  }
+  
+export const sumarPuntos = (puntosCarta : number) : number => {
+    return puntosCarta + partida.puntuacion;
+  }
+  
+export  const actualizarPuntos = (puntosSumados : number) : void => {
+    partida.puntuacion = puntosSumados;
+  }  
+
+export const comprobacionPuntuacion = (puntuacion: number) : Estado => {
+    if (puntuacion > 7.5) {
+        return "PERDEDOR";
+    }
+    if (puntuacion === 7.5 ) {
+        return "GANADOR";
+    }
+    if (puntuacion <= 4) {
+        return "MENOR_DE_CUATRO";
+    }   
+    if (puntuacion <= 5) {
+        return "CINCO";
+    }  
+    if (puntuacion <= 6 || puntuacion <= 7) {
+        return "SEIS_O_SIETE";
+    } 
+    return "PERDEDOR";
+    }  
