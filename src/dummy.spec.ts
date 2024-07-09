093ac72f044d525a6f32eb6ea3197b3b4a139269
Guste Gaubaite <219.guste@gmail.com>
2024-07-09 10:49:18 +0200

    Creada prueba unitaria para probar la función que determina si un jugador gana o pierde el juego

--- src/dummy.spec.ts
+++ src/dummy.spec.ts
@@ -1,10 +1,38 @@
-describe("dummy specs", () => {
-  it("should pass spec", () => {
+import { Estado } from './model';
+
+import {  consigueNumeroRandom, obtenerCarta, comprobacionPuntuacion } from './motor';
+
+
+import { dameCarta } from './ui';
+
+// Comprobación de si el jugador gana el juego o no
+
+describe("comprobacionPuntuacion", () => {
+  it("si la puntuación es mayor que 7.5, debería devolver PERDEDOR", () => {
     // Arrange
+    const puntuacion : number = 9;
+    const resultadoEsperado : Estado = "PERDEDOR";
 
     // Act
 
+    const resultado = comprobacionPuntuacion(puntuacion);
+
     // Assert
-    expect(true).toBeTruthy();
+    expect(resultado).toBe(resultadoEsperado);
+  });
+
+  it("si la puntuación es igual a 7.5, debería devolver GANADOR", () => {
+    // Arrange
+    const puntuacion : number = 7.5;
+    const resultadoEsperado : Estado = "GANADOR";
+
+    // Act
+
+    const resultado = comprobacionPuntuacion(puntuacion);
+
+    // Assert
+    expect(resultado).toBe(resultadoEsperado);
+
   });
 });
+
