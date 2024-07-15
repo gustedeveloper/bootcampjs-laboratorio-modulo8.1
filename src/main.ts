da5886ec15b6c3da255341ad1827392228f91638
Guste Gaubaite <219.guste@gmail.com>
2024-07-15 11:42:25 +0200

    Implementada función que extrae una lista de pacientes de Pediatria y menores de 10 años

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ import { Pacientes, pacientes } from './model';
 // a) extraer la lista de pacientes que están asignados a la especialidad de Pediatría
 
 const obtenPacientesAsignadosAPediatria = (
-    pacientes: Pacientes[]): Pacientes[] => {
+    pacientes: Pacientes[]
+): Pacientes[] => {
         let pacientesPediatria = [];
         for (let i = 0; i < pacientes.length; i++) {
             if (pacientes[i].especialidad === "Pediatra") {
@@ -18,7 +19,19 @@ const obtenPacientesAsignadosAPediatria = (
 
 console.log("Lista de pacientes de Pediatria: ", obtenPacientesAsignadosAPediatria(pacientes));
 
-// b) 
+// b) extraer la lista de pacientes asignados a Pediatría y que tengan una edad menor de 10 años
 
+const obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios = (
+    pacientes: Pacientes[]
+): Pacientes[] => {
+    let pacientesPediatriaMenoresdeDiezAnios = [];
+    for (let i = 0; i < pacientes.length; i++) {
+        if (pacientes[i].especialidad === "Pediatra" && pacientes[i].edad < 10) {
+            pacientesPediatriaMenoresdeDiezAnios.push(pacientes[i]);
+        }
+    }
+    return pacientesPediatriaMenoresdeDiezAnios;
+}
 
+console.log("Lista de pacientes de Pediatria menores de 10 años: ", obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios(pacientes));
 
