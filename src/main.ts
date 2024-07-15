a7bb752112e318c636d5d57c3b4139f1968b9036
Guste Gaubaite <219.guste@gmail.com>
2024-07-15 11:33:44 +0200

    Declarados los datos iniciales y desarrollada función que extrae una ista de pacientes asignados a especialidad Pediatria

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,24 @@
 import "./style.css";
 
-import {inicializaUI, handleButtonClicks} from './ui';
+import { Pacientes, pacientes } from './model';
 
-inicializaUI();
-handleButtonClicks();
+// Apartado 1: 
+// a) extraer la lista de pacientes que están asignados a la especialidad de Pediatría
+
+const obtenPacientesAsignadosAPediatria = (
+    pacientes: Pacientes[]): Pacientes[] => {
+        let pacientesPediatria = [];
+        for (let i = 0; i < pacientes.length; i++) {
+            if (pacientes[i].especialidad === "Pediatra") {
+                pacientesPediatria.push(pacientes[i]);
+            }
+        }
+        return pacientesPediatria;
+    }
+
+console.log("Lista de pacientes de Pediatria: ", obtenPacientesAsignadosAPediatria(pacientes));
+
+// b) 
+
+
 
